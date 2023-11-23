d221ae13b2696d6204d5553e0fd3d2d99dbc4616
Georgi Dimov <147717567+dimovv4@users.noreply.github.com>
2023-11-23 21:03:17 +0200

    Create vowel_sum.py

---
+++ vowel_sum.py
@@ -0,0 +1,16 @@
+user_input = input()
+user_input = user_input.lower()
+total_sum = 0
+for char in user_input:
+    if char in 'aeiou':
+        if char == 'a':
+            total_sum += 1
+        elif char == 'e':
+            total_sum += 2
+        elif char == 'i':
+            total_sum += 3
+        elif char == 'o':
+            total_sum += 4
+        elif char == 'u':
+            total_sum += 5
+print(total_sum)
